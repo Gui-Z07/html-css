5706dc10e918329457dfc251dadd9420203f1547
GuilhermeZampar <145402235+Gui-Z07@users.noreply.github.com>
2024-01-26 18:35:36 -0300

    estudos formularios

--- Exercicios/ex26/form.js
+++ Exercicios/ex26/form.js
@@ -1,7 +1,6 @@
-document.getElementById('telefone').addEventListener('input', function(e) {
-    var telefone = e.target.value.replace(/\D/g, '');
-    if (telefone.length === 11) {
-      telefone = telefone.replace(/^([0-9]{2})([0-9]{9})$/, '($1) $2');
-    }
-    e.target.value = telefone;
-   });
+
+
+function calcIdade(){
+let atual = new Date().getFullYear()
+iidade.innerHTML = Number(atual) - Number(iano.value)
+}
